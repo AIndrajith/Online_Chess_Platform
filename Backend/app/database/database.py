3f516a18584a122b0585b555a9f9327a5fa5e5e1
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2026-02-15 00:10:19 +0530

    Add shared SQLAlchemy Base and initialize user table schema

--- Backend/app/database/database.py
+++ Backend/app/database/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker
+from Backend.app.database.base import Base
+from urllib.parse import quote_plus
 
-DATABASE_URL = "mysql+mysqlconnector://root:12378Av@##$@localhost:3306/chess_db"
+password = quote_plus("12378Av@##$")
+DATABASE_URL = f"mysql+mysqlconnector://root:{password}@localhost:3306/chess_db"
 
 engine = create_engine(
     DATABASE_URL,
@@ -13,3 +16,7 @@ SessionLocal = sessionmaker(
     autoflush= False,
     bind=engine,
 )
+
+def init_db():          # tables are created when only init_db() is called
+    import Backend.app.models
+    Base.metadata.create_all(bind=engine)
